ba0534db20dfc22ae0480629107c866a9d38a157
Joe Chea <s.chea@aupp.edu.kh>
2023-12-28 11:26:36 +0700

    add lsp support for python, php, jsts, and lua

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,9 @@ return {
   priority = 1000,
   config = function()
     -- require("catppuccin").setup() is not required or optional here because this function is already call main setup()
+    require("catppuccin").setup({
+      flavour = "mocha",
+    })
     vim.cmd.colorscheme "catppuccin"
   end
 }
